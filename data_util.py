ba78e8ef77a0e4ac2a94a2161ec612d712069a78
devonrgraham <devonrgraham@gmail.com>
2018-05-11 07:42:36 -0700

    porting to private repo

---
+++ data_util.py
@@ -0,0 +1,226 @@
+import numpy as np
+import sqlite3
+from itertools import groupby
+
+class DataLoader:
+
+    def __init__(self, data_folder, tr, vl, ts):
+
+        self.data_folder = data_folder
+        self.team_player, self.team_match = self._load_tables(tr, vl, ts)
+
+
+    def _load_tables(self, tr, vl, ts):
+        
+        # data = 'sample'
+        data = 'soccer'
+
+        if data == 'sample':
+
+            inds0 = np.array([ [0,0],[0,2],[0,3],[1,0],[1,2],[2,1],[2,3],[3,1],[3,2],[3,3],[4,0],[4,1],[4,2],[5,1],[5,3] ])
+            vals0 = np.array([ 1,0,1,1,0,0,1,0,1,1,0,1,0,1,1 ])
+            shape0 = np.array([6,4])
+            table0 = Table(0, inds0, vals0, shape0, tr, vl, ts)
+
+
+            inds1 = np.array([ [0,0],[0,2],[1,3],[2,0],[2,4],[3,3],[3,4],[4,1],[5,1],[5,2] ])
+            vals1 = np.array([ 3,2,0.1,-3,-1,-0.1,1,-2,2,-2 ])
+            shape1 = np.array([6,5])
+            table1 = Table(1, inds1, vals1, shape1, tr, vl, ts, split_mode='by_col')
+
+        elif data == 'soccer':
+
+            entities_map = {'player':0, 'team':1, 'match':2}
+            player_map = {} ## map SQL id's to rows/cols of data matrices 
+            team_map = {}
+            match_map = {}
+
+            conn = sqlite3.connect(self.data_folder + '/database.sqlite')
+
+            ## TODO better features ...
+
+            players = conn.execute("SELECT player_api_id FROM Player ORDER BY player_api_id;").fetchall()
+            np.random.shuffle(players) 
+            for i, player in enumerate(players):
+                player_map[player[0]] = i
+            n_players = i + 1            
+
+            teams = conn.execute("SELECT team_api_id FROM Team ORDER BY team_api_id;").fetchall()
+            np.random.shuffle(teams)
+            for i, team in enumerate(teams):
+                team_map[team[0]] = i
+            n_teams = i + 1
+
+            matches = conn.execute("SELECT match_api_id, home_team_api_id, away_team_api_id, home_team_goal, away_team_goal, home_player_1, home_player_2, home_player_3, home_player_4, home_player_5, home_player_6, home_player_7, home_player_8, home_player_9, home_player_10, home_player_11, away_player_1, away_player_2, away_player_3, away_player_4, away_player_5, away_player_6, away_player_7, away_player_8, away_player_9, away_player_10, away_player_11 FROM Match;").fetchall()            
+            np.random.shuffle(matches)
+
+            team_match = []
+            team_player = []
+
+            for i, match in enumerate(matches):
+                match_id, home_team_id, away_team_id, home_team_goal, away_team_goal = match[0:5]
+                home_team_id = team_map[home_team_id]
+                away_team_id = team_map[away_team_id]
+                home_player_ids = match[5:16]
+                away_player_ids = match[16:27]
+                
+                match_map[match_id] = i
+                match_id = i
+
+                team_match.append( [home_team_id, match_id, home_team_goal-away_team_goal] )
+                team_match.append( [away_team_id, match_id, away_team_goal-home_team_goal] )
+
+                for player_id in home_player_ids:
+                    if player_id != None:
+                        team_player.append( [home_team_id, player_map[player_id], 1] )
+
+                for player_id in away_player_ids:
+                    if player_id != None:
+                        team_player.append( [away_team_id, player_map[player_id], 1] )
+            n_matches = i + 1
+
+            team_match.sort()
+            team_player.sort()
+
+            temp = []
+            for key, group in groupby(team_player, key=lambda x: x[0:2]):
+                key.append(sum([g[2] for g in group]))
+                temp.append(key)
+            
+            team_player = temp
+
+            team_player = np.array(team_player)
+            team_match = np.array(team_match)
+
+            table0 = Table(0, team_player[:,0:2], team_player[:,2], np.array([n_teams, n_players]), 1. - vl/tr, vl/tr, .0)
+            table1 = Table(1, team_match[:,0:2], team_match[:,2], np.array([n_teams, n_matches]), tr, vl, ts, split_mode='by_col')
+
+            conn.close()
+
+        return table0, table1
+
+
+
+
+class Table:
+
+    def __init__(self, tid, indices, values, shape, tr, vl, ts, split_mode='uniform'):
+
+        assert tr+vl+ts == 1, "(" + str(tr) + ", " + str(vl) + ", " + str(ts) + ") is not a valid train/valid/test split"
+        n = values.shape[0]
+
+        if split_mode == 'uniform':
+            n_ts = int(n*ts)
+            n_vl = int(n*vl)
+            n_tr = n - n_ts - n_vl
+            split = np.concatenate((np.zeros(n_tr, np.int32), np.ones(n_vl, np.int32), 2*np.ones(n_ts, np.int32)))
+            np.random.shuffle(split)
+        
+        elif split_mode == 'by_col': # So that the two entries for each match are in the same tr/vl/ts split 
+            n_cols = shape[1]
+            n_cols_ts = int(n_cols*ts)
+            n_cols_vl = int(n_cols*vl)
+            n_cols_tr = n_cols - n_cols_ts - n_cols_vl
+            col_split = np.concatenate((np.zeros(n_cols_tr, np.int32), np.ones(n_cols_vl, np.int32), 2*np.ones(n_cols_ts, np.int32)))
+            np.random.shuffle(col_split)
+            split = np.take(col_split, indices[:,1])
+
+            n_ts = np.sum(split == 2)
+            n_vl = np.sum(split == 1)
+            n_tr = n - n_ts - n_vl
+
+
+            # split = np.concatenate((np.zeros(n_tr, np.int32), np.ones(n_vl, np.int32), 2*np.ones(n_ts, np.int32)))
+            # np.random.shuffle(split)       
+
+            # print('split: ', np.sum(split))
+            # print('AA_split: ', np.sum(AA_split))
+
+
+
+        elif split_mode == 'by_row':
+            print("TODO: implement")
+
+        self._set_data_splits(indices, values, split)
+        self.shape = shape
+        self.mean_tr = np.mean(self.values_tr)
+        self.num_values_tr = n_tr
+        self.num_values = n
+        
+
+    def _set_data_splits(self, indices, values, split):
+        self.indices_all = indices
+        self.indices_tr = indices[split == 0]
+        self.indices_vl = indices[split == 1]
+        self.indices_tr_vl = indices[split <= 1]
+        self.indices_ts = indices[split == 2]
+
+        self.values_all = values
+        self.values_tr = values[split == 0]
+        self.values_vl = values[split == 1]
+        self.values_tr_vl = values[split <= 1]
+        self.values_ts = values[split == 2]
+
+        self.split = split
+
+
+    # def transpose(self):
+    #     trans = np.concatenate((self.indices_all, self.values_all[:,None], self.split[:,None]), axis=1)
+    #     trans[:,[0,1,2,3]] = trans[:,[1,0,2,3]]        
+    #     trans = list(trans)
+    #     trans.sort(key=lambda row: row[0])
+    #     trans = np.array(trans)
+
+    #     inds = trans[:,0:2]
+    #     vals = trans[:,2]
+    #     split = trans[:,3]
+
+    #     self._set_data_splits(inds, vals, split)
+
+    #     self.shape[[0,1]] = self.shape[[1,0]]
+
+
+
+
+
+
+
+
+
+# # For debugging
+# def sparse_array_to_dense(indices, values, shape, num_features):
+#     out = np.zeros(list(shape) + [num_features])
+#     inds = expand_array_indices(indices, num_features)
+#     inds = list(zip(*inds))
+#     out[inds] = values
+#     return out
+
+# # For debugging
+# def expand_array_indices(indices, num_features):    
+#     num_vals = indices.shape[0]
+#     inds_exp = np.reshape(np.tile(range(num_features), reps=[num_vals]), newshape=[-1, 1]) # expand dimension of mask indices
+#     inds = np.tile(indices, reps=[num_features,1]) # duplicate indices num_features times
+#     inds = np.reshape(inds, newshape=[num_features, num_vals, 2])
+#     inds = np.reshape(np.transpose(inds, axes=[1,0,2]), newshape=[-1,2])
+#     inds = np.concatenate((inds, inds_exp), axis=1)
+#     return inds
+
+# def sparse_transpose(indices, values, shape):
+#     trans = np.concatenate((indices, values[:,None]), axis=1)
+#     trans[:,[0,1,2]] = trans[:,[1,0,2]]
+#     trans = list(trans)
+#     trans.sort(key=lambda row: row[0])
+#     trans = np.array(trans)    
+#     inds = trans[:,0:2]
+#     vals = trans[:,2]
+#     shape[[0,1]] = shape[[1,0]]
+#     return {'indices':inds, 'values':vals, 'shape':shape}
+
+
+
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,305 @@
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from copy import deepcopy
+from data_util import DataLoader, Table
+from model import Model
+
+## Noise mask has 0's corresponding to values to be predicted
+def table_rmse_loss(values, values_rec, noise_mask):
+    non_noise = tf.cast(1 - noise_mask, tf.float32)
+    return tf.sqrt( tf.reduce_sum(((values - values_rec)**2)*non_noise) / (tf.reduce_sum(non_noise) + 1e-10) )
+
+
+def table_prediction_accuracy(indices, values, values_rec, shape, noise):
+    # print("HERE")
+    # print(np.squeeze(sparse_array_to_dense(indices, values, shape, 1)))
+
+
+    pred_d = np.sum( ((values < opts['draw_eps']) * (values_rec < opts['draw_eps']))[noise == 0] )
+    inds_wl = (noise == 0) * (values_rec >= opts['draw_eps'])
+    pred_wl = np.sum((values_rec*values)[inds_wl] > 0 )
+    
+    return (pred_d + pred_wl) / (2*np.sum(noise == 0))
+
+
+def make_noise_mask(noise_rate, num_vals):
+    """A 0/1 noise mask. 0's correspond to dropped out values."""
+    n0 = int(noise_rate * num_vals)
+    n1 = num_vals - n0
+    noise_mask = np.concatenate((np.zeros(n0), np.ones(n1)))
+    np.random.shuffle(noise_mask)
+    return noise_mask
+
+# def make_noisy_values(values, noise_rate, noise_value):
+#     """replace noise_rate fraction of values with noise_value."""
+#     num_vals = values.shape[0]
+#     noise = make_noise_mask(noise_rate, num_vals) 
+#     values_noisy = np.copy(values)
+#     values_noisy[noise == 0] = noise_value
+#     return noise, values_noisy
+
+
+
+# For debugging
+def sparse_array_to_dense(indices, values, shape, num_features):
+    out = np.zeros(list(shape) + [num_features])
+    inds = expand_array_indices(indices, num_features)
+    inds = list(zip(*inds))
+    out[inds] = values
+    return out
+
+def expand_array_indices(indices, num_features):    
+    num_vals = indices.shape[0]
+    inds_exp = np.reshape(np.tile(range(num_features), reps=[num_vals]), newshape=[-1, 1]) # expand dimension of mask indices
+    inds = np.tile(indices, reps=[num_features,1]) # duplicate indices num_features times
+    inds = np.reshape(inds, newshape=[num_features, num_vals, 2])
+    inds = np.reshape(np.transpose(inds, axes=[1,0,2]), newshape=[-1,2])
+    inds = np.concatenate((inds, inds_exp), axis=1)
+    return inds
+
+def sparse_transpose(indices, values, shape, split):
+    trans = np.concatenate((indices, values[:,None], split[:,None]), axis=1)
+    trans[:,[0,1,2,3]] = trans[:,[1,0,2,3]]
+    trans = list(trans)
+    trans.sort(key=lambda row: row[0])
+    trans = np.array(trans)    
+    inds = trans[:,0:2]
+    vals = trans[:,2]
+    split = trans[:,3]
+    shape[[0,1]] = shape[[1,0]]
+    return {'indices':inds, 'values':vals, 'shape':shape, 'split':split}
+
+
+
+
+def main(opts):
+    gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.9)
+
+    tr, vl, ts = opts['data_split']
+
+    if opts['debug']:
+        np.random.seed(12345)
+
+    data = DataLoader('data', tr, vl, ts)
+
+    # print(np.squeeze(sparse_array_to_dense(data.team_match.indices_all, data.team_match.values_all, data.team_match.shape, 1)))
+    # print("")
+    # print(np.squeeze(sparse_array_to_dense(data.team_match.indices_tr, data.team_match.values_tr, data.team_match.shape, 1)))
+    # print("")
+    # print(np.squeeze(sparse_array_to_dense(data.team_match.indices_vl, data.team_match.values_vl, data.team_match.shape, 1)))
+    # print("")
+    # print(np.squeeze(sparse_array_to_dense(data.team_match.indices_tr_vl, data.team_match.values_tr_vl, data.team_match.shape, 1)))
+
+
+    with tf.Graph().as_default():
+
+        if opts['debug']:
+            tf.set_random_seed(12345)
+
+        model = Model(**opts['layer_opts'])
+
+        team_player = {}
+        team_player['indices'] = tf.placeholder(tf.int32, shape=(None, 2), name='team_player_indices')
+        team_player['values'] = tf.placeholder(tf.float32, shape=(None), name='team_player_values_noisy')
+        team_player['shape'] = data.team_player.shape
+        team_player_noise_mask = tf.placeholder(tf.float32, shape=(None), name='team_player_noise_mask')
+        team_player_values = tf.placeholder(tf.float32, shape=(None), name='team_player_values')
+
+        team_match = {}
+        team_match['indices'] = tf.placeholder(tf.int32, shape=(None, 2), name='team_match_indices')        
+        team_match['values'] = tf.placeholder(tf.float32, shape=(None), name='team_match_values_noisy')
+        team_match['shape'] = data.team_match.shape
+        team_match_noise_mask = tf.placeholder(tf.float32, shape=(None), name='team_match_noise_mask')
+        team_match_values = tf.placeholder(tf.float32, shape=(None), name='team_match_values')
+
+
+        team_player_out_tr, team_match_out_tr = model.get_output(team_player, team_match)
+        team_player_out_vl, team_match_out_vl = model.get_output(team_player, team_match, reuse=True)
+
+        rec_loss_tr = 0
+        # rec_loss_tr += table_rmse_loss(team_player_values, team_player_out_tr['values'], team_player_noise_mask)
+        rec_loss_tr += table_rmse_loss(team_match_values, team_match_out_tr['values'], team_match_noise_mask)
+        reg_loss = sum(tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES))
+        total_loss_tr = rec_loss_tr + opts['regularization_rate']*reg_loss
+
+        rec_loss_vl = 0
+        # rec_loss_vl += table_rmse_loss(team_player_values, team_player_out_vl['values'], team_player_noise_mask)
+        rec_loss_vl += table_rmse_loss(team_match_values, team_match_out_vl['values'], team_match_noise_mask)
+
+        train_step = tf.train.AdamOptimizer(opts['learning_rate']).minimize(total_loss_tr)
+        # train_step = tf.train.GradientDescentOptimizer(opts['learning_rate']).minimize(total_loss_tr)
+        sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
+        sess.run(tf.global_variables_initializer())
+
+
+        if opts['layer_opts']['pool_mode'] == 'mean':
+            noise_value = 0
+        if opts['layer_opts']['pool_mode'] == 'max':
+            noise_value = -1e10
+
+        losses_tr = []
+        losses_vl = []
+        losses_vl_mean = []
+        accuracies_tr = []
+        accuracies_vl = []
+
+        accuracy_vl_best = 0
+        accuracy_vl_best_ep = 0
+
+
+        for ep in range(opts['epochs']):
+            if opts['verbosity'] > 0:
+                print('------- epoch:', ep, '-------')
+
+            # team_player_noise = make_noise_mask(opts['noise_rate'], data.team_player.num_values_tr) 
+            # team_player_values_noisy = np.copy(data.team_player.values_tr)
+            # team_player_values_noisy[team_player_noise == 0] = noise_value
+
+            team_player_noise = np.ones_like(data.team_player.values_tr)
+            team_player_values_noisy = np.copy(data.team_player.values_tr)
+
+
+            team_match_noise = make_noise_mask(opts['noise_rate'], data.team_match.num_values_tr) 
+            team_match_values_noisy = np.copy(data.team_match.values_tr)
+            team_match_values_noisy[team_match_noise == 0] = noise_value
+
+            tr_dict = {team_player['indices']:data.team_player.indices_tr, 
+                       team_player['values']:team_player_values_noisy, # noisy values
+                       team_player_noise_mask:team_player_noise,
+                       team_player_values:data.team_player.values_tr, # clean values
+
+                       team_match['indices']:data.team_match.indices_tr, 
+                       team_match['values']:team_match_values_noisy, # noisy values
+                       team_match_noise_mask:team_match_noise,
+                       team_match_values:data.team_match.values_tr # clean values
+                      }
+
+
+            _, loss_tr, team_match_vals_out_tr, = sess.run([train_step, total_loss_tr, team_match_out_tr['values']], feed_dict=tr_dict)
+            losses_tr.append(loss_tr)
+
+                       
+            pred_accuracy_tr = table_prediction_accuracy(data.team_match.indices_tr, data.team_match.values_tr, team_match_vals_out_tr, data.team_match.shape, team_match_noise)
+            accuracies_tr.append(pred_accuracy_tr)
+
+
+
+            # team_player_noise = 1 - data.team_player.split[data.team_player.split <= 1]
+            # team_player_values_noisy = data.team_player.values_tr_vl
+            # team_player_values_noisy[team_player_noise == 0] = noise_value
+
+            team_player_noise = np.ones_like(data.team_player.values_tr_vl)
+            team_player_values_noisy = np.copy(data.team_player.values_tr_vl)
+
+            team_match_noise = 1 - data.team_match.split[data.team_match.split <= 1]
+            team_match_values_noisy = np.copy(data.team_match.values_tr_vl)
+            team_match_values_noisy[team_match_noise == 0] = noise_value
+
+            vl_dict = {team_player['indices']:data.team_player.indices_tr_vl, 
+                       team_player['values']:team_player_values_noisy, # noisy values
+                       team_player_noise_mask:team_player_noise,
+                       team_player_values:data.team_player.values_tr_vl, # clean values
+
+                       team_match['indices']:data.team_match.indices_tr_vl, 
+                       team_match['values']:team_match_values_noisy, # noisy values
+                       team_match_noise_mask:team_match_noise,
+                       team_match_values:data.team_match.values_tr_vl # clean values
+                      }
+            
+            loss_vl, team_match_vals_out_vl = sess.run([rec_loss_vl, team_match_out_vl['values']], feed_dict=vl_dict)            
+            losses_vl.append(loss_vl)
+
+
+
+            mean_tr = data.team_match.mean_tr
+            non_noise = 1 - team_match_noise
+            loss_vl_mean = np.sqrt( np.sum(((data.team_match.values_tr_vl - mean_tr)**2)*non_noise) / np.sum(non_noise) )
+            losses_vl_mean.append(loss_vl_mean)
+
+            split = data.team_match.split
+            in_t = sparse_transpose(data.team_match.indices_tr_vl, data.team_match.values_tr_vl, data.team_match.shape, split[split < 2])
+            out_t = sparse_transpose(data.team_match.indices_tr_vl, team_match_vals_out_vl, data.team_match.shape, split[split < 2])
+            vals_in_t = in_t['values']
+            vals_out_t = out_t['values']
+            
+            noise = 1 - in_t['split']
+            print(vals_in_t[noise == 0][0:20])
+            print(np.round(vals_out_t[noise == 0][0:20]).astype(np.int32))
+            print(vals_out_t[noise == 0][0:20])
+
+
+            # print(np.round(vals_out_t[split == 0][0:20]).astype(np.int32))
+            # print(vals_out_t[split == 0][0:20])
+
+
+            # print(vals_in[team_match_noise == 1][0:20])
+            # print(np.round(vals_out[team_match_noise == 1][0:20]).astype(np.int32))
+            # print('')
+            # print(vals_in[team_match_noise == 0][0:20])
+            # print(np.round(vals_out[team_match_noise == 0][0:20]).astype(np.int32))
+
+
+            pred_accuracy_vl = table_prediction_accuracy(data.team_match.indices_tr_vl, data.team_match.values_tr_vl, team_match_vals_out_vl, data.team_match.shape, team_match_noise)
+            accuracies_vl.append(pred_accuracy_vl)
+
+
+            if pred_accuracy_vl > accuracy_vl_best:
+                accuracy_vl_best = pred_accuracy_vl
+                accuracy_vl_best_ep = ep
+                ## TODO save model
+
+            
+            if opts['verbosity'] > 0:
+                # print("epoch {:5d}. training loss: {:5.15f} \t validation loss: {:5.5f} \t predicting mean: {:5.5f} \t train accuracy rate: {:5.5f} \t val accuracy rate: {:5.5f}".format(ep+1, loss_tr, loss_vl, loss_vl_mean, pred_accuracy_tr, pred_accuracy_vl))
+                print("epoch {:5d}. train accuracy rate: {:5.5f} \t val accuracy rate: {:5.5f} \t best val accuracy rate: {:5.5f} at epoch {:5d}".format(ep+1, pred_accuracy_tr, pred_accuracy_vl, accuracy_vl_best, accuracy_vl_best_ep+1))
+
+        
+
+        show_last = 1000
+        plt.title('RMSE Loss')
+        plt.plot(range(opts['epochs'])[-show_last:], losses_tr[-show_last:], '.-', color='blue')
+        plt.plot(range(opts['epochs'])[-show_last:], losses_vl[-show_last:], '.-', color='green')
+        plt.plot(range(opts['epochs'])[-show_last:], losses_vl_mean[-show_last:], '.-', color='red')
+        plt.xlabel('epoch')
+        plt.ylabel('RMSE')
+        plt.legend(('training', 'validation', 'mean'))
+        # plt.show()
+        plt.savefig("rmse.pdf", bbox_inches='tight')
+        plt.clf()
+
+        plt.title('Prediction Accuracy')
+        plt.plot(range(opts['epochs'])[-show_last:], accuracies_tr[-show_last:], '.-', color='blue')
+        plt.plot(range(opts['epochs'])[-show_last:], accuracies_vl[-show_last:], '.-', color='green')
+        plt.plot(range(opts['epochs'])[-show_last:], [.46 for _ in range(opts['epochs'])], '.-', color='red')
+        plt.plot(range(opts['epochs'])[-show_last:], [.53 for _ in range(opts['epochs'])], '.-', color='yellow')
+        plt.xlabel('epoch')
+        plt.ylabel('Accuracy')
+        plt.legend(('training prediction', 'validation prediction', 'baseline', 'experts'))        
+        # plt.show()
+        plt.savefig("pred.pdf", bbox_inches='tight')
+
+
+
+if __name__ == "__main__":
+    np.set_printoptions(suppress=True,linewidth=np.nan,threshold=np.nan)
+
+    opts = {'epochs':300,
+            'data_split':[.8, .2, .0], # train, validation, test split
+            'noise_rate':.2, # match vl/tr or ts/(tr+vl) 
+            'regularization_rate':.00001,
+            'learning_rate':.001,
+            'draw_eps':1e-2, # how close to be considered a draw
+            'layer_opts':{'pool_mode':'mean',
+                          'activation':tf.nn.relu,
+                         },
+            'debug':True,
+            'verbosity':2
+            }
+    main(opts)
+
+
+
+
+
+
